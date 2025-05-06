19fd9f67996f9d2df46b05cb4a4a90252596635e
syahvan <syahvanalviansyah91@gmail.com>
2025-05-06 09:49:13 +0700

    Bump version: 0.1.0 → 0.2.0

--- supertracker/__init__.py
+++ supertracker/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Syahvan Alviansyah"""
 __email__ = "syahvanalviansyah91@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from supertracker.detection.core import Detections
 from supertracker.detection.utils import box_iou_batch
